e1c8b9fc66c0ffd3458266c06233de2dab157efa
phiphiphi2k1 <philonnni@gmail.com>
2024-05-31 21:16:43 +0700

    update: icon

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BeanFast',
+      title: 'BeanFast Admin',
       theme: AppTheme.defaulTheme,
       initialRoute: "/",
       getPages: [
